f89a84a00aa99b9f656e53f3e0f79c21ffa57cb7
TheNetherWatcher <117659673+TheNetherWatcher@users.noreply.github.com>
2024-01-28 22:07:03 +0530

    Create code.cpp

---
+++ Lab_3/Ques_2/code.cpp
@@ -0,0 +1,80 @@
+/*
+
+
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣶⣿⣿⣷⣶⣄⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣾⣿⣿⡿⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⡟⠁⣰⣿⣿⣿⡿⠿⠻⠿⣿⣿⣿⣿⣧⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⠏⠀⣴⣿⣿⣿⠉⠀⠀⠀⠀⠀⠈⢻⣿⣿⣇⠀⠀⠀
+⠀⠀⠀⠀⢀⣠⣼⣿⣿⡏⠀⢠⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⡀⠀⠀
+⠀⠀⠀⣰⣿⣿⣿⣿⣿⡇⠀⢸⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀
+⠀⠀⢰⣿⣿⡿⣿⣿⣿⡇⠀⠘⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⢀⣸⣿⣿⣿⠁⠀⠀
+⠀⠀⣿⣿⣿⠁⣿⣿⣿⡇⠀⠀⠻⣿⣿⣿⣷⣶⣶⣶⣶⣶⣿⣿⣿⣿⠃⠀⠀⠀
+⠀⢰⣿⣿⡇⠀⣿⣿⣿⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀
+⠀⢸⣿⣿⡇⠀⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠉⠛⠛⠛⠉⢉⣿⣿⠀⠀⠀⠀⠀⠀
+⠀⢸⣿⣿⣇⠀⣿⣿⣿⠀⠀⠀⠀⠀⢀⣤⣤⣤⡀⠀⠀⢸⣿⣿⣿⣷⣦⠀⠀⠀
+⠀⠀⢻⣿⣿⣶⣿⣿⣿⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣦⡀⠀⠉⠉⠻⣿⣿⡇⠀⠀
+⠀⠀⠀⠛⠿⣿⣿⣿⣿⣷⣤⡀⠀⠀⠀⠀⠈⠹⣿⣿⣇⣀⠀⣠⣾⣿⣿⡇⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣦⣤⣤⣤⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⢿⣿⣿⣿⣿⣿⣿⠿⠋⠉⠛⠋⠉⠉⠁⠀⠀
+
+
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+#define ll long long
+#define ulli unsigned long long int
+#define ull unsigned long long
+#define rp(a, b, c) for(int a = b; i < b + c; a++)
+#define out(a) cout<<a<<endl
+#define in(a) cin>>a
+#define all(x) (x).begin(), (x).end()
+#define fr(a, b) for(int i = a; i < b; i++)
+#define mod 1000000007
+#define pb push_back 
+const ll int inf=9223372036854775807;
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define vi vector<int>
+#define sva(v) sort(all(v))
+#define sz(x) int((x).size())
+const lli N=10e6+10;
+// use incase of input and output file
+// freopen(input.txt,r,stdin);
+// freopen(output.txt,w,stdout);
+
+void solve()
+{
+    int x; vector<int> a;
+    ifstream inp("./input.txt");
+    while(inp >> x) a.pb(x);
+    inp.close();
+
+    int ans = INT_MIN, comp = 0;
+    for (int i = 0; i < a.size(); i++) {
+        comp = comp + a[i];
+        if (ans < comp)
+            ans = comp;
+ 
+        if (comp < 0)
+            comp = 0;
+    }
+
+    ofstream outputFile("output.txt");
+	outputFile << ans;
+    outputFile.close();
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    //cin>>t;
+    while(t--) solve();
+}
